4f9148fec9b9dd68df08207ac5f8ba2cf43762b0
Сорокин Алексей <jiysuan@mail.ru>
2022-08-10 16:19:53 +0400

    Сделал задания... кажется

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+void Zadacha41()
+    {
+    Console.Write("Сколько чисел будете вводить? ");
+    int M = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[M];
+    FillAndSortArray(array);
+    }
+
+    void FillAndSortArray(int[] array, int temp = 0)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Введите число: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                if (array[i] > 0) temp++;
+            }
+        Console.WriteLine("Количество числе больше нуля, введенных пользователем: " + temp);
+        }
+
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+void Zadacha43()
+    {
+    Console.Write("Укажите переменную b первой прямой: ");
+    double b1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Укажите переменную b второй прямой: ");
+    double b2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Укажите угловой коэффициент первой прямой: ");
+    double k1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Укажите угловой коэффициент второй прямой: ");
+    double k2 = Convert.ToDouble(Console.ReadLine());
+    double x = 0;
+    double y = 0;
+
+    if (k1 == k2)
+        {
+            Console.WriteLine("Прямые параллельны");
+        }
+    else
+        {
+        x = Math.Round((b2-b1)/(k1-k2), 2);
+        y = Math.Round((k1*x) + b1, 2);
+        }
+
+    Console.WriteLine("Точка пересечения прямых: ("+x+"; "+y+")" );
+    }
+//Zadacha41();
+Zadacha43();
